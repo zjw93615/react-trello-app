47880ce407c3b84a83b4dd07a06dfa4458b8df10
JiaweiZheng <zjw93615@163.com>
2019-03-12 11:59:14 +1100

    first commit

---
+++ src/components/TodoItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class TodoItem extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Hellow World</h3>
+      </div>
+    )
+  }
+}
+
+export default TodoItem
---
+++ src/components/Todos.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import TodoItem from './TodoItem'
+
+class Todos extends Component {
+  render() {
+    return this.props.todos.map((todo) => (<TodoItem />));
+  }
+}
+
+export default Todos;
